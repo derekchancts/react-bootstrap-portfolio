d264c0d6e13f54f0b83c3f1d20309346ca48e0a6
derekchancts <derekchancts@yahoo.com.hk>
2021-09-10 17:17:57 +0800

    Navbar complated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <>
+    <Navbar />
+   
+    </>
   );
 }
 
